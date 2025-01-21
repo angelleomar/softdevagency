79f03803f32a34499d3d69af9e66c537839f4fab
angelleomar <snokert17@hotmail.com>
2025-01-21 14:59:18 -0600

    Add files via upload

--- assets/javascripts/guardardark.js
+++ assets/javascripts/guardardark.js
@@ -38,3 +38,24 @@ document.addEventListener('DOMContentLoaded', () => {
     // Añadir evento al botón
     darkModeButton.addEventListener('click', toggleDarkMode);
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const isWebView = () => {
+        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        // Detecta si está en un WebView en Android o iOS
+        return (
+            (/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(userAgent)) || // iOS WebView
+            (/\bwv\b/.test(userAgent) || /Android.*Version\/[0-9.]+/.test(userAgent)) // Android WebView
+        );
+    };
+
+    // Si está en WebView, oculta el apartado de descarga de la app
+    if (isWebView()) {
+        const downloadSection = document.getElementById('download-app');
+        if (downloadSection) {
+            downloadSection.style.display = 'none'; // Ocultar sección
+        }
+    }
+});
