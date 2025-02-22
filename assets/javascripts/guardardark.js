c062d4bdc538854c1a645861b6f398f33fdcdf10
angelleomar <snokert17@hotmail.com>
2025-02-22 01:34:40 -0600

    Add files via upload

--- assets/javascripts/guardardark.js
+++ assets/javascripts/guardardark.js
@@ -50,3 +50,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 });
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && (event.key === "u" || event.key === "s" || event.key === "i" || event.key === "j")) {
+    event.preventDefault();
+  }
+});
